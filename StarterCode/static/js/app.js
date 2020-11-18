4a94460392d4c1bfa47e21787a4e3037308b5172
Kumba Ye <kumba@Kumbas-MacBook-Pro.local>
2020-11-17 21:22:06 -0500

    adding gauge files

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,4 @@
-function BuildStart() {
+function BuildStart(variableID) {
   d3.json("data/samples.json").then(function (data) {
     console.log(data);
     console.log(data)
@@ -11,11 +11,12 @@ function BuildStart() {
     })
     BuildBarChart(data, variableID);
     BuildBubbleChart(data, variableID);
-    BuildDemographicInfo(data, variableID)
+    BuildDemographicInfo(data, variableID);
+    BuildGaugeChart(data, variableID)
   }
   )
 };
-BuildStart();
+BuildStart(940);
 
 function BuildDemographicInfo(data, variableID){
   var metademo= data.metadata.filter(demo=>demo.id== variableID)[0];
@@ -101,7 +102,7 @@ function BuildBubbleChart(data,variableID) {
     title: 'Top 10 OTUs found',
     showlegend: false,
     height: 600,
-    width: 600
+    width: 1200
   };
   Plotly.newPlot('bubblechart', data, layout);
 
@@ -121,6 +122,7 @@ function Buildingfunction(variableID) {
     BuildBarChart(data, variableID);
     BuildBubbleChart(data, variableID);
     BuildDemographicInfo(data, variableID);
+    BuildGaugeChart(data, variableID);
   }
 
   )
@@ -128,4 +130,59 @@ function Buildingfunction(variableID) {
 
 
 
+function BuildGaugeChart(data, variableID){
+  var metademo= data.metadata.filter(demo=>demo.id== variableID)[0];
+  var frequency= metademo.wfreq;
 
+
+
+  var data = [
+    {
+        type: 'pie',
+        showlegend: false,
+        hole: 0.4,
+        rotation: 90,
+        values: [ 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81],
+        text: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
+        direction: 'clockwise',
+        textinfo: 'text',
+        textposition: 'inside',
+        marker: {
+          colors: ['','','','','','','','','','white'],
+          labels: ['0-1','1-2','2-3','3-4','4-5','5-6','6-7','7-8','8-9'],
+          hoverinfo: 'label'
+        
+      }
+    }
+  ];
+  
+
+    
+    var degrees = 50, radius = 0.9
+    var radians = degrees * Math.PI / 180
+    console.log(radians)
+    var x = -1 * radius * Math.cos(radians) * frequency
+    var y = radius * Math.sin(radians)
+    console.log(x);
+    console.log(y);
+    console.log(frequency);
+    var gaugeLayout = {
+      shapes: [{
+        type: 'line',
+        x0:0.5,
+        y0:0.5,
+        x1: 0.6,
+        y1: 0.6,
+        line: {
+          color: 'black',
+          width: 3
+        }
+      }],
+      title: 'Chart',
+      xaxis: {visible: false, range: [-1, 1]},
+      yaxis: {visible: false, range: [-1, 1]}
+    }
+
+    Plotly.plot('gauge', data, gaugeLayout)
+
+};
